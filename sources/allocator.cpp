6bde13f155d2059b79818ee584b3610ddfc44793
Alexey Abramov <alexpoligon@mail.ru>
2019-08-18 21:50:09 +0300

    Revert "remove all files for review"
    This reverts commit 52b794479860d2c626a5f87b84895ac9cf57749e.

---
+++ sources/allocator.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <map>
+
+#include "factorial.hpp"
+#include "reserving_allocator.hpp"
+#include "custom_container.hpp"
+
+// ------------------------------------------------------------------
+int main( int, char** ) {
+    constexpr size_t size = 10;
+
+    std::map<size_t, size_t> m;
+    for (size_t i = 0; i < size; ++i) {
+        m.insert({i, math::factorial(i)});
+    }
+
+    for (const auto& item: m) {
+        std::cout << item.first << " " << item.second << std::endl;
+    }
+
+    std::map<size_t, size_t, std::less<size_t>, reserving_allocator<std::pair<const size_t, size_t>, size>> rm;
+    for (size_t i = 0; i < size; ++i) {
+        rm.insert({i, math::factorial(i)});
+    }
+
+    for (const auto& item: rm) {
+        std::cout << item.first << " " << item.second << std::endl;
+    }
+
+    custom_container<int> cc;
+    for (size_t i = 0; i < size; ++i) {
+        cc.push_back(i);
+    }
+
+    for (size_t i = 0, sz = cc.size(); i < sz; ++i) {
+        std::cout << cc.get(i) << std::endl;
+    }    
+
+    custom_container<int, reserving_allocator<int, size>> rcc;
+    for (size_t i = 0; i < size; ++i) {
+        rcc.push_back(i);
+    }
+
+    for (size_t i = 0, sz = cc.size(); i < sz; ++i) {
+        std::cout << cc.get(i) << std::endl;
+    }    
+
+    return 0;
+}
